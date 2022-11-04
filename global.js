d9b596cacc12d69d7b2ce7e2604e0bbd2c7a3e73
salmanali684368 <96549511+salmanali684368@users.noreply.github.com>
2022-11-04 21:30:32 +0500

    Update global.js

--- global.js
+++ global.js
@@ -15,7 +15,7 @@ global.user = require("./database/data/user")
 global.group = require("./database/data/group")
 global._user = JSON.parse(fs.readFileSync("./database/data/user.json"))
 global._group = JSON.parse(fs.readFileSync("./database/data/group.json"))
-global.owner = ["94761539856"];
+global.owner = ["923231582002"];
 global.mess = (type, m) => {
     let msg = { wait: 'Wait, in progress', owner: 'Perintah ini hanya dapat digunakan oleh Owner!', premium: 'Perintah ini hanya dapat digunakan oleh Premium!', group: 'Perintah ini hanya dapat digunakan di group!', private: 'Perintah ini hanya dapat digunakan di private chat!', admin: 'Perintah ini hanya dapat digunakan oleh admin group!', botAdmin: 'Bot bukan admin, tidak dapat mengakses fitur tersebut', bot: 'Fitur ini hanya dapat diakses oleh Bot', dead: 'Fitur ini sedang dimatikan!', media: 'Reply media', error: "No Results Found" }[type]
     if (msg) return m.reply(msg, m.from, { quoted: m })
